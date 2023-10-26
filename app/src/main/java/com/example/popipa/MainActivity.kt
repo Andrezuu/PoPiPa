c7b35d9c69a1e108b2c06bbcdecd321157798786
Jhonatan <sellooricalcos@gmail.com>
2023-10-25 22:08:38 -0400

    Entrar a la galeria del dispositivo.

--- app/src/main/java/com/example/popipa/MainActivity.kt
+++ app/src/main/java/com/example/popipa/MainActivity.kt
@@ -2,21 +2,37 @@ package com.example.popipa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val selectImageButton = findViewById<Button>(R.id.imagenSelect)
+        selectImageButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            startActivityForResult(intent, 1)
+
+
+        }
     }
-//<<<<<<< HEAD
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
-
-    //Hola ahlahdlkdjakdjjkjalk
-    //Bota al Jhoni XD
-//=======
-    //Me lees Andres aaaaaa
-    //Me lees Adrian aaaaaa
-//>>>>>>> origin/master
-
-    //jhfkfjhfkjfhkhkajf primer push
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            val selectedImageUri = data?.data
+            if (selectedImageUri != null) {
+                val imageView = findViewById<ImageView>(R.id.imagenGale)
+                imageView.setImageURI(selectedImageUri)
+                //Glide.with(this).load(selectedImageUri).into(imageView)
+            }
+        }
+    }
 }
